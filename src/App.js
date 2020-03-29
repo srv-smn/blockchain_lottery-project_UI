e17609396945d3d5ccad8ba5bf678e0ffeeb399b
sourav suman <saurav2fly@gmail.com>
2020-03-29 08:44:18 +0530

    successfully setup web3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 function App() {
+  console.log(web3.version);
   return (
     <div className="App">
       <header className="App-header">
