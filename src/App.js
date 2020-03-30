d95afde219d104875be0c518f6fbc213995e8a92
sourav suman <saurav2fly@gmail.com>
2020-03-30 08:07:59 +0530

    metamask connected with web3 and found the account number on web browser

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
-function App() {
-  console.log(web3.version);
+import lottery from './lottery';
+
+class App extends Component {
+constructor(props) {
+  super(props);
+  this.state={manager: '' };
+}
+
+async componentDidMount() {
+  const manager = await lottery.methods.manager().call();
+  this.setState({manager});
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <h2>Lottery Contract</h2>
+  <p>This contract is manager by {this.state.manager}</p>
+  </div>
   );
+}
 }
 
 export default App;
