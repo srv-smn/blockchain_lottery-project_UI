563af71132b1bf510fa68a818c4fb9b59b1b1999
sourav suman <saurav2fly@gmail.com>
2020-07-19 14:23:18 +0530

    build the UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
+import { Card, Header, Segment, Form, Container ,Button } from 'semantic-ui-react'
 
 class App extends Component {
 
@@ -20,6 +21,56 @@ async componentDidMount() {
   const balance = await web3.eth.getBalance(lottery.options.address);
   this.setState({manager, players, balance});
 }
+HeaderExampleFloating(){
+    return (<Segment clearing style = {{marginTop : '20px'}} >
+    <Header as='h2' color='blue' textAlign ='center'>
+      LOTTERY CONTRACT
+    </Header>
+
+  </Segment>);
+
+}
+
+renderForm(){
+
+}
+
+renderCards(){
+  const items = [
+    {
+      header:  this.state.manager,
+      description:
+        'Owner/Creator of the lottery',
+      meta:'MANAGER',
+      color: 'red',
+      doubling: true,
+      centered: true,
+      style:{overflowWrap: 'break-word'},
+    },
+    {
+      header: this.state.players.length,
+      description:
+        'Total participants in the lottery',
+      meta: 'TOTAL PARTICIPANTS',
+      color: 'yellow',
+      centered: true
+
+    },
+    {
+      header: web3.utils.fromWei(this.state.balance, 'ether'),
+      description:
+        'Total monry in current lottery',
+      meta: 'TOTAL MONEY',
+      color: 'blue',
+      centered: true
+
+    },
+  ]
+  return <Card.Group centered items={items} style = {{marginTop : '10px'}}/>;
+}
+
+
+
 onSubmit = async event =>{
   event.preventDefault();
     await window.ethereum.enable() ;
@@ -30,6 +81,7 @@ onSubmit = async event =>{
     value: web3.utils.toWei(this.state.value, 'ether')
     });
     this.setState({message: 'You have been entered !'});
+    window.location.reload(false);
   };
 
 onClick = async () =>{
@@ -40,34 +92,42 @@ onClick = async () =>{
     from: accounts[0]
   });
   this.setState({message: 'A Winner has been picked '});
+  window.location.reload(false);
 };
 
 
 render() {
   return (
   <div>
-  <h2>Lottery Contract</h2>
-  <p>This contract is manager by {this.state.manager}.
-    There are currently {this.state.players.length} people entered copmetating to win {web3.utils.fromWei(this.state.balance, 'ether')} ether
+  <p>
+  {this.HeaderExampleFloating()}
   </p>
-<hr />
-
-<form onSubmit = {this.onSubmit}>
-<h4>Want to try your Luck ?</h4>
-<div>
-<label>Amount of ether to enter </label>
-<input
-value = {this.state.value}
-onChange = {event => this.setState({value :event.target.value})}
-/>
+  <p>
+  {this.renderCards()}
+  </p>
+  <Container style = {{marginTop : '30px'}}>
+  <Card  centered= {true} color = 'yellow' centered ={true} fluid ={true}>
+  <Form onSubmit = {this.onSubmit} size ="big" >
+  <p><h2 align = 'center' color='blue'>Want to try luck ?</h2></p>
+          <Form.Input
+            placeholder='Amount in ETH'
+            name='name'
+            value={this.state.value}
+            onChange={event => this.setState({value :event.target.value})}
+          />
+          <div>
+<Button type='submit' positive ={true} fluid ={true}>Submit</Button>
 </div>
-<button>Enter</button>
-</form>
-<hr />
-<h4>Ready to pick a winner?</h4>
-<button onClick ={this.onClick}>Pick a winner !</button>
-<hr />
-<h1>{this.state.message}</h1>
+</Form>
+</Card>
+</Container>
+<Container style = {{marginTop : '30px'}}>
+<Card  centered= {true} color = 'yellow' centered ={true} fluid ={true} >
+<p><h2 align = 'center' color='blue'>Ready to pick a winner?</h2></p>
+<Button type='submit' positive ={true} fluid ={true} onClick ={this.onClick}>Pick a winner !</Button>
+<h5>{this.state.message}</h5>
+</Card>
+</Container>
 
 
 
